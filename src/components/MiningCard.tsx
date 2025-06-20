b9517f0ae468ecdc0bf56ca914200b3f6672e5d1
Odusanya Temiloluwa <Odusanya.Temiloluwa2001@gmail.com>
2025-06-20 06:37:12 +0000

    change only the "mining" in the progress bar to "Generating"

--- src/components/MiningCard.tsx
+++ src/components/MiningCard.tsx
@@ -156,7 +156,7 @@ const MiningCard: FC<MiningCardProps> = ({ onCoinsClaimed, level }) => {
     if (isMining) {
       return (
         <>
-          <Zap className="mr-2 h-5 w-5 animate-pulse-lg text-black" /> Mining... ({Math.round(miningProgress)}%)
+          <Zap className="mr-2 h-5 w-5 animate-pulse-lg text-black" /> Generating... ({Math.round(miningProgress)}%)
         </>
       );
     }
